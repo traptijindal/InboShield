7ea04bbbd82e13fb2d164e7ac2b1e6014aae0f7e
traptijindal <traptijindal5678@gmail.com>
2025-04-28 11:02:35 +0530

    u[dated app.py

--- app.py
+++ app.py
@@ -76,5 +76,9 @@ def home():
 
     return render_template("index.html", result=None)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # take PORT from environment or use 5000 locally
+    app.run(host="0.0.0.0", port=port)
